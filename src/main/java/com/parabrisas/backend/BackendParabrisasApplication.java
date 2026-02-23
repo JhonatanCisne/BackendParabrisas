21a47925bf6df36d648f7fa8a1fb52c0d51c8527
Jhonatan Cisneros <Jhonatanjorge151@gmail.com>
2026-02-22 19:48:17 -0500

    primer commit

---
+++ src/main/java/com/parabrisas/backend/BackendParabrisasApplication.java
@@ -0,0 +1,13 @@
+package com.parabrisas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendParabrisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendParabrisasApplication.class, args);
+	}
+
+}
